84620de69e1ed14a668f678b28c4070199232b6a
unknown <1217113694@qq.com>
2016-05-14 13:22:22 +0800

    merge and fixed router

--- src/router/driver/driverRoutes.js
+++ src/router/driver/driverRoutes.js
@@ -1,53 +1,53 @@
 export default function (router) {
   // driver map
   router.map({
-    '/driverHome': {
-      component: require()
-    },
-    '/search/company': {
-      component: require()
-    },
-    '/search/goods': {
-      component: require()
-    },
+    // '/driverHome': {
+    //   component: require()
+    // },
+    // '/search/company': {
+    //   component: require()
+    // },
+    // '/search/goods': {
+    //   component: require()
+    // },
     '/createTransit': {
-      component: require()
+      component: require('components/driver/CreateTransit.vue')
     },
-    '/driverTransitList': {
-      component: require()
-    },
-    '/driverTransitList/:id': {
-      component: require()
-    },
-    'driverTransitList/filter': {
-      component: require()
-    },
-    'createRefuel': {
-      component: require()
-    },
-    'driverRefuel': {
-      component: require()
-    },
-    'driverRefuelList': {
-      component: require()
-    },
-    'driverRefuelList/:id': {
-      component: require()
-    },
-    'driverRefuelList/filter': {
-      component: require()
-    },
-    'createRepairCar': {
-      component: require()
-    },
-    'driverRepairCarList': {
-      component: require()
-    },
-    'driverRepairCarList/:id': {
-      component: require()
-    },
-    'driverRepairCarList/filter': {
-      component: require()
+    // '/driverTransitList': {
+    //   component: require()
+    // },
+    // '/driverTransitList/:id': {
+    //   component: require()
+    // },
+    // '/driverTransitList/filter': {
+    //   component: require()
+    // },
+    '/createRefuel': {
+      component: require('src/components/driver/CreateRefuel.vue')
     }
+    // '/driverRefuel': {
+    //   component: require()
+    // },
+    // '/driverRefuelList': {
+    //   component: require()
+    // },
+    // '/driverRefuelList/:id': {
+    //   component: require()
+    // },
+    // '/driverRefuelList/filter': {
+    //   component: require()
+    // },
+    // '/createRepairCar': {
+    //   component: require()
+    // },
+    // '/driverRepairCarList': {
+    //   component: require()
+    // },
+    // '/driverRepairCarList/:id': {
+    //   component: require()
+    // },
+    // '/driverRepairCarList/filter': {
+    //   component: require()
+    // }
   })
 }
